b602780c2d1a774fefe6d58f307a2b2defb1e589
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2023-01-04 18:52:07 -0500

    add nix-shell.nix

---
+++ shells/nix-shell.nix
@@ -0,0 +1,10 @@
+# Shell for bootstrapping flake-enabled nix and home-manager and sops
+# You can enter it through 'nix develop' or (legacy) 'nix-shell'
+
+{ pkgs ? (import ./nixpkgs.nix) { } }: {
+  default = pkgs.mkShell {
+    # Enable experimental features without having to specify the argument
+    NIX_CONFIG = "experimental-features = nix-command flakes";
+    nativeBuildInputs = with pkgs; [ nix home-manager git age gnupg sops ssh-to-pgp ssh-to-age ];
+  };
+}
