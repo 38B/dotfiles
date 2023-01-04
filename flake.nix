b8f22758ba4f289a69618a98fbf19cea7b6bf7f9
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2023-01-04 18:47:16 -0500

    add flake.nix based on flake-parts

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "a collection of my system, user, and shell delarations";
+
+  inputs = {
+
+    # NixOS Generators
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # NixOS Hardware
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+
+    # Flake Parts
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Home Manager
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Sops Nix
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs @ { flake-parts, ... }:
+    (flake-parts.lib.evalFlakeModule
+      { inherit inputs; }
+      {
+        imports = [
+          ./hosts/crumb
+          ./hosts/sippet
+          ./homes/blob
+          ./homes/muck
+          ./shells/nix-shell.nix
+        ];
+        systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
+        perSystem = { inputs', ... }: {
+          # make pkgs available to all `perSystem` functions
+          _module.args.pkgs = inputs'.nixpkgs.legacyPackages;
+        };
+      }).config.flake;
+}
