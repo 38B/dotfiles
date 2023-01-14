c923137e8b73177928270827c149b6565fdfc02f
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2023-01-14 18:52:22 +0000

    add hyprland config through home-manager

--- homes/_modules/hyprland/default.nix
+++ homes/_modules/hyprland/default.nix
@@ -8,6 +8,6 @@
       MOZ_ENABLE_WAYLAND = "1";
       WLR_NO_HARDWARE_CURSORS= "1";
     };
-    file.".config/hypr/hyprland.conf".source = ./hyprland.conf
+    file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
   };
 }
