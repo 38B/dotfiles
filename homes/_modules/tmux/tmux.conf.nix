5562432def8f4abe7c3c9ed9367ebb747548d145
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2023-01-15 13:32:41 +0000

    configure tmux

--- homes/_modules/tmux/tmux.conf.nix
+++ homes/_modules/tmux/tmux.conf.nix
@@ -9,15 +9,15 @@ bind -r L resize-pane -R 2
 set -g status-style 'bg=colour8 fg=colour0 dim'
 set -g status-left-length 30
 set -g status-justify centre
-set -g status-left '#[fg=colour9,bg=colour0]«#I›#W#[fg=colour8] #H #[fg=colour8,bg=colour0]░▒#[fg=colour0,bg=colour8]░'
+set -g status-left '#[fg=colour9,bg=colour0]«#I›#[fg=colour8]#W #[fg=colour8,bg=colour0]░▒#[fg=colour0,bg=colour8]░'
 set -g status-right-length "100"
 set -g status-right '░▒#[fg=colour8,bg=colour0]░  %a %B %-d, %Y'
 set -g pane-border-style 'fg=colour3 bg=colour0'
 set -g pane-active-border-style 'bg=colour0 fg=colour9'
-set -g window-status-format '#[fg=colour8,bg=colour0]░ #W #F ░'
-set -g window-status-separator "▒"
-setw -g window-status-current-style 'fg=colour1 bg=colour0 bold'
-setw -g window-status-current-format '░ #W #F ░'
+set -g window-status-format ''
+set -g window-status-separator ""
+setw -g window-status-current-style ''
+setw -g window-status-current-format ''
 
 setw -g monitor-activity on
 set -g visual-activity on
