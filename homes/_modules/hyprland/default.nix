9381ed5ece9f50decc6bd2f5e16d728fd9e2dc96
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2023-02-11 19:37:43 +0000

    Hyperland updates

--- homes/_modules/hyprland/default.nix
+++ homes/_modules/hyprland/default.nix
@@ -8,7 +8,7 @@
       MOZ_ENABLE_WAYLAND = "1";
       WLR_NO_HARDWARE_CURSORS= "1";
     };
-    sessionPath = "$HOME/.local/bin";
+    sessionPath = { "$HOME/.local/bin"; };
     file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
     file.".local/bin/hypr".source = ./hypr.sh;
   };
