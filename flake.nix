8575bd0f7915dc3a79aaf13c020b10ee70a55cb8
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2023-02-25 21:17:06 +0000

    nix update && try user sops

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
           ./hosts
           ./homes
 	  ./shells
-	  inputs.sops-nix.modules.home-manager.sops
+	  inputs.sops-nix.homeManagerModules.sops
         ]; 
         systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
         perSystem = { inputs', ... }: {
